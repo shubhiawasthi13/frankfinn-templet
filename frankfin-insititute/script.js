2ca2aed04a53006c35d7955be58f760a3f2b3d65
shubhi awasthi <119504808+shubhiawasthi13@users.noreply.github.com>
2023-07-06 19:11:38 +0530

    Add files via upload

---
+++ frankfin-insititute/script.js
@@ -0,0 +1,25 @@
+menubtn = document.querySelector(".menu-btn");
+menubtn.onclick = function(){
+  navBar = document.querySelector(".nav-container");
+  navBar.classList.toggle("active");
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  setTimeout(showSlides, 5000);
+}
